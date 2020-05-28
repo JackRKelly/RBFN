120d64c57a29bd4c6d5c69f594c23bce77402c96
MyManVan <kcjackkelly@gmail.com>
2020-05-28 11:55:50 -0500

    Added past speakers list styling

--- client/src/pages/Speakers/speakers.tsx
+++ client/src/pages/Speakers/speakers.tsx
@@ -9,6 +9,20 @@ const Speakers: FC = () => {
   return (
     <main>
       <h1>Speakers</h1>
+      <ul className="past-speakers">
+        <li>
+          <h5>Speaker Name</h5>
+          <h6>05/28/2020 - 17 Days ago</h6>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda
+            ipsum quidem sint, odit labore aliquid? Suscipit omnis dolorum nobis
+            ipsam!
+          </p>
+          <div className="button-container">
+            <button>More Details</button>
+          </div>
+        </li>
+      </ul>
     </main>
   );
 };
