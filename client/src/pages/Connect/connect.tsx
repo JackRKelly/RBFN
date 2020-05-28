d3d88e516c9dbd2462ed2bbf93178261c2c1af88
MyManVan <kcjackkelly@gmail.com>
2020-05-28 10:46:53 -0500

    Added connect list elements

--- client/src/pages/Connect/connect.tsx
+++ client/src/pages/Connect/connect.tsx
@@ -9,6 +9,16 @@ const Connect: FC = () => {
   return (
     <main>
       <h1>Connect</h1>
+      <ul className="connection-list">
+        <li>
+          <div className="card-header">
+            <h5>@keithkcvc</h5>
+          </div>
+
+          <p>Revenue Based Finance Network Founder's Twitter</p>
+          <button>View Page</button>
+        </li>
+      </ul>
     </main>
   );
 };
