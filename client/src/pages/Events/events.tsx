7ce783665a4c9e03121b342a8743ccd70a4f04e8
MyManVan <kcjackkelly@gmail.com>
2020-05-28 11:49:24 -0500

    Styled events page and added button hover effects

--- client/src/pages/Events/events.tsx
+++ client/src/pages/Events/events.tsx
@@ -9,6 +9,21 @@ const Events: FC = () => {
   return (
     <main>
       <h1>Events</h1>
+      <ul className="upcoming-events">
+        <li>
+          <h5>Event Name</h5>
+          <h6>05/28/2020 - In 17 Days</h6>
+          <h6>123456 S Street St.</h6>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda
+            ipsum quidem sint, odit labore aliquid? Suscipit omnis dolorum nobis
+            ipsam!
+          </p>
+          <div className="button-container">
+            <button>More Details</button>
+          </div>
+        </li>
+      </ul>
     </main>
   );
 };
