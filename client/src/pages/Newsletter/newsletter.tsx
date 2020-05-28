224db1f7150b320a1642bd62d54a0decd3171d71
MyManVan <kcjackkelly@gmail.com>
2020-05-28 10:28:16 -0500

    Added recent editions section

--- client/src/pages/Newsletter/newsletter.tsx
+++ client/src/pages/Newsletter/newsletter.tsx
@@ -9,6 +9,20 @@ const Newsletter: FC = () => {
   return (
     <main>
       <h1>Newsletter</h1>
+      <h3>Recent editions:</h3>
+      <ul className="recent-editions">
+        <li>
+          <h5>05/28/2020 - 17 Days ago</h5>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda
+            ipsum quidem sint, odit labore aliquid? Suscipit omnis dolorum nobis
+            ipsam!
+          </p>
+          <div className="button-container">
+            <button>Read More</button>
+          </div>
+        </li>
+      </ul>
     </main>
   );
 };
