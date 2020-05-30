b5759a9143be4e95f3b75088f3f291d1d902b9fd
MyManVan <kcjackkelly@gmail.com>
2020-05-30 11:43:42 -0500

    Added blog type interface for api response

--- client/src/pages/Blog/blog.tsx
+++ client/src/pages/Blog/blog.tsx
@@ -1,15 +1,28 @@
 import React, { FC, useEffect, useState } from "react";
 import "./blog.scss";
 
+interface Blog {
+  banner: Array<Object>;
+  content: string;
+  title: string;
+  createdAt: string;
+  id: string;
+  updatedAt: string;
+  __v: number;
+  _id: string;
+}
+
 const Blog: FC = () => {
-  const [blogs, setBlogs] = useState([]);
+  const [blogs, setBlogs] = useState<Array<Blog>>();
 
   useEffect(() => {
     document.title = "Blog | RBFN";
 
     fetch("http://localhost:1337/blogs").then((res) => {
-      res.json().then((json) => {
+      res.json().then((json: Array<Blog>) => {
         setBlogs(json);
+        console.log(json);
+        console.log(new Date(json[0].createdAt));
       });
     });
   }, []);
@@ -19,15 +32,11 @@ const Blog: FC = () => {
       <h1>Blog</h1>
       <h2>Past blog posts:</h2>
       <ul className="blog-posts">
-        {blogs.map((blog) => (
-          <li>
+        {blogs?.map((blog, index) => (
+          <li key={index}>
             <h5>{blog.title}</h5>
-            <h6>05/28/2020 - 17 Days ago</h6>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Assumenda ipsum quidem sint, odit labore aliquid? Suscipit omnis
-              dolorum nobis ipsam!
-            </p>
+            <h6>{blog.createdAt} - 17 Days ago</h6>
+            <p>{blog.content}</p>
             <div className="button-container">
               <button>Read More</button>
             </div>
