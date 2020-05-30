d9eed03041584c9dbad3fb0a11217c15100e8b45
MyManVan <kcjackkelly@gmail.com>
2020-05-30 11:32:22 -0500

    Added api fetch to blog route in blog page

--- client/src/pages/Blog/blog.tsx
+++ client/src/pages/Blog/blog.tsx
@@ -1,9 +1,17 @@
-import React, { FC, useEffect } from "react";
+import React, { FC, useEffect, useState } from "react";
 import "./blog.scss";
 
 const Blog: FC = () => {
+  const [blogs, setBlogs] = useState([]);
+
   useEffect(() => {
     document.title = "Blog | RBFN";
+
+    fetch("http://localhost:1337/blogs").then((res) => {
+      res.json().then((json) => {
+        setBlogs(json);
+      });
+    });
   }, []);
 
   return (
@@ -11,18 +19,20 @@ const Blog: FC = () => {
       <h1>Blog</h1>
       <h2>Past blog posts:</h2>
       <ul className="blog-posts">
-        <li>
-          <h5>Blog post title</h5>
-          <h6>05/28/2020 - 17 Days ago</h6>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda
-            ipsum quidem sint, odit labore aliquid? Suscipit omnis dolorum nobis
-            ipsam!
-          </p>
-          <div className="button-container">
-            <button>Read More</button>
-          </div>
-        </li>
+        {blogs.map((blog) => (
+          <li>
+            <h5>{blog.title}</h5>
+            <h6>05/28/2020 - 17 Days ago</h6>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Assumenda ipsum quidem sint, odit labore aliquid? Suscipit omnis
+              dolorum nobis ipsam!
+            </p>
+            <div className="button-container">
+              <button>Read More</button>
+            </div>
+          </li>
+        ))}
       </ul>
     </main>
   );
