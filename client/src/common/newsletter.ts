25786b3ab780c3c5ea6078abb2469bfce3ef5e4c
MyManVan <kcjackkelly@gmail.com>
2020-06-01 12:04:04 -0500

    Moved banner to common strapi file

--- client/src/common/newsletter.ts
+++ client/src/common/newsletter.ts
@@ -1,34 +1,4 @@
-interface Banner {
-  alternativeText: string;
-  caption: string;
-  createdAt: string;
-  ext: string;
-  formats: {
-    large: {
-      url: string;
-    };
-    medium: {
-      url: string;
-    };
-    small: {
-      url: string;
-    };
-    thumbnail: {
-      url: string;
-    };
-  };
-  hash: string;
-  height: number;
-  id: string;
-  mime: string;
-  name: string;
-  provider: string;
-  related: Array<string>;
-  size: number;
-  updatedAt: string;
-  url: string;
-  width: number;
-}
+import { Banner } from "./strapi";
 
 export interface NewsletterT {
   banner: Banner;
