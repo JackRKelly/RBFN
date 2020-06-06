ee4cf513bc866ad07d9151e943bfaac1a00b56ee
MyManVan <kcjackkelly@gmail.com>
2020-06-06 09:48:05 -0500

    Added recent newsletter card to home summary

--- client/src/pages/Home/home.tsx
+++ client/src/pages/Home/home.tsx
@@ -9,6 +9,7 @@ import "./home.scss";
 import { SpeakerT } from "../../common/speaker";
 import { differenceDate, formatDate } from "../../common/date";
 import { Link } from "react-router-dom";
+import { NewsletterT } from "../../common/newsletter";
 
 interface Props {
   setLoading: Dispatch<SetStateAction<boolean>>;
@@ -17,7 +18,7 @@ interface Props {
 const Home: FC<Props> = (props) => {
   const { setLoading } = props;
   const [speaker, setSpeaker] = useState<SpeakerT>();
-  const [newsletter, setNewsletter] = useState();
+  const [newsletter, setNewsletter] = useState<NewsletterT>();
   const [event, setEvent] = useState();
   const [blog, setBlog] = useState();
 
@@ -27,7 +28,13 @@ const Home: FC<Props> = (props) => {
 
     fetch("http://localhost:1337/speakers").then((res) => {
       res.json().then((json: Array<SpeakerT>) => {
-        setSpeaker(json[0]);
+        setSpeaker(json.reverse()[0]);
+        setLoading(false);
+      });
+    });
+    fetch("http://localhost:1337/newsletters").then((res) => {
+      res.json().then((json: Array<NewsletterT>) => {
+        setNewsletter(json.reverse()[0]);
         setLoading(false);
       });
     });
@@ -57,11 +64,38 @@ const Home: FC<Props> = (props) => {
               </div>
             </div>
           ) : (
-            <> </>
+            <div className="loading-card"> </div>
+          )}
+        </div>
+        <div className="recent-newsletter">
+          <h3>Recent Newsletter:</h3>
+          {newsletter ? (
+            <div className="newsletter-card">
+              <h5>{newsletter.title}</h5>
+              <h6>
+                {differenceDate(newsletter.createdAt) * -1 === 1
+                  ? "Posted Yesterday"
+                  : differenceDate(newsletter.createdAt) * -1 === 0
+                  ? "Posted Today"
+                  : `${formatDate(newsletter.createdAt)} - 
+                  ${differenceDate(newsletter.createdAt) * -1} days ago`}
+              </h6>
+              <p>{newsletter.description.substring(0, 120)}...</p>
+              <div className="button-container">
+                <a
+                  href={`http://localhost:1337${newsletter.pdf.url}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View Newsletter
+                </a>
+              </div>
+            </div>
+          ) : (
+            <div className="loading-card"> </div>
           )}
         </div>
 
-        <h3>Recent Newsletter:</h3>
         <h3>Upcoming Event:</h3>
         <h3>Recent Blog:</h3>
       </div>
