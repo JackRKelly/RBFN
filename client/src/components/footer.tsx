b490c00aa2f476c595ed04a46b8a25dbc2d57351
JackRKelly <kcjackkelly@gmail.com>
2020-08-15 12:48:09 -0500

    Added footer component

---
+++ client/src/components/footer.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+
+const Footer: FC = () => {
+  return (
+    <footer>
+      <div className="row-1"></div>
+      <div className="row-2"></div>
+      <div className="row-3"></div>
+    </footer>
+  );
+};
+
+export default Footer;
