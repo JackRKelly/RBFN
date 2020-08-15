1a6a26cd50518dbf79e4acf2b2f14540dc39e4ff
JackRKelly <kcjackkelly@gmail.com>
2020-08-15 13:27:29 -0500

    Added footer columns and styling

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -3,13 +3,42 @@ import React, { FC } from "react";
 const Footer: FC = () => {
   return (
     <footer>
-      <div className="row">
-        <h3>About</h3>
+      <div className="footer-wrapper">
+        <div className="row row-1">
+          <h2>About Us</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Dignissimos, dolorem accusantium! Soluta, beatae laborum cumque
+            itaque placeat rerum voluptas reprehenderit blanditiis veritatis
+            tempora aut similique illum mollitia quisquam ab, ea dolores
+            sapiente odit dolorum quidem. Aperiam porro inventore odio dolores
+            non possimus repudiandae voluptatum quam corrupti illum soluta
+            voluptatibus maxime cupiditate, iure aliquam nisi facilis tempore
+            omnis dolorem quidem, odit explicabo dicta repellat nam. Officia,
+            rem consectetur repudiandae dolore numquam ex dignissimos sit est
+            doloribus molestias, quaerat, reiciendis labore vitae aut sequi unde
+            repellendus iste? Quia, accusantium. Accusantium, commodi maiores!
+            Vitae tempore libero impedit cum laudantium ab id sequi harum.
+          </p>
+        </div>
+        <div className="row row-2">
+          <h2>Contact</h2>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="mailto:email@email.com"
+          >
+            email@email.com
+          </a>
+          <ul>
+            <li>Facebook</li>
+            <li>Twitter</li>
+          </ul>
+        </div>
+        <div className="row row-3">
+          <h2>3rd Column</h2>
+        </div>
       </div>
-      <div className="row">
-        <h3>Contact</h3>
-      </div>
-      <div className="row">sd</div>
     </footer>
   );
 };
